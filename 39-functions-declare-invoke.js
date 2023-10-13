c1e5311c5b1dc2df1aa1dceb5513a6f2f0ec0db8
John-Davidson-8 <jodo828@gmail.com>
2023-10-13 11:15:03 +0100

    Feat: 39-Functions-Declare-Invoke

---
+++ 39-functions-declare-invoke.js
@@ -0,0 +1,20 @@
+// Functions Declare and Invoke
+
+function hello() {
+  // logic and declare the function
+  console.log("Hello there Bob");
+  console.log("Hello there Anna");
+  console.log("Hello there Susy");
+}
+
+// Now we invoke the function
+
+hello();
+// some code here
+hello();
+// some code here
+hello();
+// some code here
+hello();
+
+// If I want to change anything in the code logic I can just do it inside the function logic. In this instance I can change any of the names.
