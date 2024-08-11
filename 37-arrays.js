cf732aeb7212b488a7bc7007cf9bb7ea6abc06a6
John-Davidson-8 <jodo828@gmail.com>
2024-08-11 13:24:03 +0100

    37 Arrays

---
+++ 37-arrays.js
@@ -0,0 +1,13 @@
+// Arrays, Functions and Objects
+// Arrays - [], 0 index based
+// Store and access data
+// Example array below
+const friends = ["john", "peter", "bob", "susy", 45, undefined, null];
+// Access item out of array see below
+console.log(friends[3]);
+// can assign value from array and assign to another variable as below
+let bestFriend = friends[2];
+// Can change the value of an item in the array also, see below
+friends[4] = "anna";
+console.log(friends[4]);
+console.log(bestFriend);
