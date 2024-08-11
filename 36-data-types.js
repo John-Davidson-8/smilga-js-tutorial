bd1cca45e54788f6772874ac995bff7e89efbacf
John-Davidson-8 <jodo828@gmail.com>
2024-08-11 12:14:07 +0100

    chapter 36 - data types in js

---
+++ 36-data-types.js
@@ -0,0 +1,26 @@
+// Data Types - 7 total
+// Primitive - String, Number, Boolean, Null, Undefined, Symbol
+// Object - Arrays, Functions, Objects
+
+// typeof - operator (typeof variable) (typeof value)
+
+// String
+const text = "some text";
+// Number
+const newNum = 45;
+// Boolean
+let value1 = true;
+let value2 = false;
+// Null
+const result = null;
+// Undefined
+let name;
+// Symbol (ES6)
+console.log(typeof text);
+console.log(typeof "hello");
+console.log(typeof newNum);
+console.log(typeof value1);
+console.log(typeof value2);
+// See below for null; this is a bug in JS where null returns as an object. it is not.
+console.log(typeof null);
+console.log(typeof name);
--- 43-functions-challenge.js
+++ 43-functions-challenge.js
@@ -25,3 +25,11 @@ const order2 = calculateTotal(50, 5);
 const order3 = calculateTotal(25, 5);
 
 console.log(order1, order2, order3);
+
+// Below is the code as function expression which is anonymous. To name this function expression I would put a name just before the parenthesis (subTotal, tax).
+
+// const calculateTotal = function(subTotal, tax) {
+//   const total = subTotal + tax;
+//   return total;
+//   return subTotal + tax;
+// }
