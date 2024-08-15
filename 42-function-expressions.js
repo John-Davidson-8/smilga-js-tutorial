d1253340e301624dd2328be647f6f6b5b5a8397d
John-Davidson-8 <jodo828@gmail.com>
2024-08-15 14:14:23 +0100

    complete function challenge

--- 42-function-expressions.js
+++ 42-function-expressions.js
@@ -12,7 +12,7 @@ function addValues(num1, num2) {
 const firstValue = addValues(3, 4);
 const secondValue = addValues(12, 34);
 
-// function expression below: the whole function from above is now assigned to the variable 'add.' The name of function addValues can be omitted as the the function is referred to as 'add.' The below function is now an 'anonymous function.' The variable 'add' holds the function.
+/* function expression below: the whole function from above is now assigned to the variable 'add.' The name of function addValues can be omitted as the the function is referred to as 'add.' The below function is now an 'anonymous function.' The variable 'add' holds the function. Also, the function name 'addValue' can be omitted as variable "add" holds the name. This creates an anonymous functions*/
 
 const add = function addValues(num1, num2) {
   return num1 + num2;
@@ -21,3 +21,8 @@ const add = function addValues(num1, num2) {
 const thirdValue = add(5, 6);
 const values = [firstValue, secondValue, thirdValue];
 console.log(values);
+
+// ES6 came up with a shortcut for functions. Below is an example:
+
+const multiply = (num1, num2) => num1 * num2;
+/*the last bit after the arrow is the function calculation and return, the first part in parenthesis is the function parameters */
--- 43-functions-challenge.js
+++ 43-functions-challenge.js
@@ -13,18 +13,18 @@
 
 // Below is one way, with the other way commented out. The first way is Smilga's preferred method for newbies as it is more explicit. However, as one becomes more adept at JS the second way that is commented out is preferred.
 
-function calculateTotal(subTotal, tax) {
-  // const total = subTotal + tax;
-  // return total;
-  return subTotal + tax;
-}
+// function calculateTotal(subTotal, tax) {
+// const total = subTotal + tax;
+// return total;
+//   return subTotal + tax;
+// }
 // console.log(calculateTotal(200, 20));
 
-const order1 = calculateTotal(100, 10);
-const order2 = calculateTotal(50, 5);
-const order3 = calculateTotal(25, 5);
+// const order1 = calculateTotal(100, 10);
+// const order2 = calculateTotal(50, 5);
+// const order3 = calculateTotal(25, 5);
 
-console.log(order1, order2, order3);
+// console.log(order1, order2, order3);
 
 // Below is the code as function expression which is anonymous. To name this function expression I would put a name just before the parenthesis (subTotal, tax).
 
@@ -33,3 +33,13 @@ console.log(order1, order2, order3);
 //   return total;
 //   return subTotal + tax;
 // }
+
+function calculateTotal(subTotal, tax) {
+  return subTotal + tax;
+}
+
+const order1 = calculateTotal(300, 30);
+const order2 = calculateTotal(400, 40);
+const order3 = calculateTotal(500, 50);
+
+console.log(order1, order2, order3);
