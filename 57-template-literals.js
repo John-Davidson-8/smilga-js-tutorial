05d33ddfc0ee058f943205d8e656ef2a60abe218
John-Davidson-8 <jodo828@gmail.com>
2024-08-26 16:08:05 +0100

    strings challenge

--- 57-template-literals.js
+++ 57-template-literals.js
@@ -9,3 +9,33 @@ console.log(sentence);
 
 const value = `Hey it's ${name} and he is ${age} years old and ${4 * 4}`;
 console.log(value);
+
+// Chapter 58 — Strings Challenge
+// below is correct but not the way he did it
+
+/*
+function fullName(firstName, lastName) {
+  const name = `your name is ${firstName.toUpperCase()}  ${lastName.toUpperCase()}`;
+  return name;
+}
+const endName = fullName("Jack", "Smith");
+console.log(endName);
+*/
+
+// function fullName(firstName, lastName) {
+//   const fullName = `${firstName} ${lastName}`;
+//   return fullName.toUpperCase();
+// }
+
+// Below is different because we are putting an object in the function parameters, note the curly brackets on the first line (they are keys in an object)
+
+console.log(fullName("john", "smith"));
+
+function fullName({ firstName, lastName }) {
+  const fullName = `${firstName} ${lastName}`;
+  return fullName.toUpperCase();
+}
+
+console.log(fullName({ lastName: "jordan", firstName: "peter" }));
+
+// There is a choice here of using parameters or objects. With objects the order you put them in the log is irrelevant, but with arguments the order has to be logical!
