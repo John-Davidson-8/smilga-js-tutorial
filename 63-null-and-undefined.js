8c220fa6781059bbb94dde0978d9aadebfbd4119
John-Davidson-8 <jodo828@gmail.com>
2024-09-08 21:03:53 +0100

    63 null and undefined

---
+++ 63-null-and-undefined.js
@@ -0,0 +1,10 @@
+// Null and Undefined
+// both represent "no value"
+
+// undefined — "js can't find value for this"
+
+// variable without value
+// missing function arguments
+// missing object properties
+
+// null - "developer sets the value"
