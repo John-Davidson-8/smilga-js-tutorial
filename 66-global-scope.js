2ead179905aa39b3df6f8f534bc01cc10e6293ca
John-Davidson-8 <jodo828@gmail.com>
2024-09-11 12:53:31 +0100

    add global scope

---
+++ 66-global-scope.js
@@ -0,0 +1,4 @@
+// Global Scope vs Local Scope
+// any variable outside code block {} is said to be in global scope
+// can be accessed anywhere in the programme
+// Gotchas: name collisions, modify by mistake
