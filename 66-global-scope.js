261a73ab2d2b99dfce1796507091a73d0af3d01c
John-Davidson-8 <jodo828@gmail.com>
2024-09-11 14:40:07 +0100

    67 global scope

--- 66-global-scope.js
+++ 66-global-scope.js
@@ -2,3 +2,21 @@
 // any variable outside code block {} is said to be in global scope
 // can be accessed anywhere in the programme
 // Gotchas: name collisions, modify by mistake
+
+let name1 = "bobo";
+name1 = "peter";
+console.log(name1);
+
+function calculate() {
+  // some other code
+  console.log(name1);
+  name1 = "orange";
+  function inner() {
+    name1 = "inner function name value";
+    console.log(`this is from inner function ${name1}`);
+  }
+  inner();
+}
+calculate();
+
+// be careful with this
